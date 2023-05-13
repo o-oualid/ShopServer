11b3b0090a5cc3e253464bc554662ea397c95b24
oualid ouazrou <o-oualid@outlook.com>
2023-05-13 15:15:09 +0100

    implement user DTOs

---
+++ src/main/kotlin/com/oualid/shopserver/dtos/UserDto.kt
@@ -0,0 +1,26 @@
+package com.oualid.shopserver.dtos
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+
+data class UserDto(
+    val id: Long, val firstName: String,
+    val lastName: String,
+    val email: String,
+    @Email
+    val password: String,
+    val role: String
+)
+
+data class UserCreationDto(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    @Email
+    val password: String
+)
+
+data class UserLoginDto(
+    @Email val email: String,
+    @NotEmpty val password: String
+)
